35d939d90440fe7a8c481c04f657ba1b53cb042c
Dann Ormond <dann.ormond@gmail.com>
2013-04-28 08:14:14 -0600

    Added logging to the update script

--- update.sh
+++ update.sh
@@ -5,9 +5,9 @@ SCRIPT_LOC=$(dirname $0)
 cd $SCRIPT_LOC/src
 source ../bin/activate
 
-python wtp_crawler.py
+python wtp_crawler.py > $SCRIPT_LOC/log 2>&1
 
 cd ..
-git add data/wtp.json
-git commit -m "Periodic data run"
-git push
+git add data/wtp.json >> $SCRIPT_LOC/log 2>&1
+git commit -m "Periodic data run" >> $SCRIPT_LOC/log 2>&1
+git push >> $SCRIPT_LOC/log 2>&1
